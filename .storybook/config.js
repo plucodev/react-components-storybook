7676796058a6bb2b37e070c57d5ec4724ed1c646
plucodev <paololucanodeveloper@gmail.com>
2019-05-15 14:39:40 -0400

    Updated config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,16 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import { addParameters } from "@storybook/react";
+import { themes } from "@storybook/theming";
+
+// Option defaults.
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+});
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context("../stories", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
